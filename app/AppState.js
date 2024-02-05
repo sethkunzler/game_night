180da518de65db1c93ae4f45167c6b26dba85d17
Seth Kunzler <sethkunzler@gmail.com>
2024-02-05 16:07:25 -0700

    create an instance

--- app/AppState.js
+++ app/AppState.js
@@ -1,10 +1,14 @@
+import { Player } from "./models/Player.js"
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
   /**@type {import('./models/Example.js').Example[]} */
-  examples = []
+  example = []
+
+  /**@type {import('./models/Player.js').Player[]} */
+  players = [new Player("Jake")]
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
---
+++ app/models/Player.js
@@ -0,0 +1,7 @@
+export class Player {
+  constructor(name) {
+    this.name = name;
+    // The Score is defined as Zero to start
+    this.score = 0;
+  }
+}
